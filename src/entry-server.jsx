a7bdf6dbbd68e3279daa4a2c80bd4928a7bea85a
Ian McPhail <webguyian@gmail.com>
2024-03-31 15:47:49 -0400

    Add Google fonts and favicon

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
+import { createHandler, StartServer } from '@solidjs/start/server';
 
 export default createHandler(() => (
   <StartServer
@@ -8,7 +8,14 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Where in the world?</title>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300;6..12,400;6..12,600;6..12,700&display=swap"
+            rel="stylesheet"
+          />
           {assets}
         </head>
         <body>
